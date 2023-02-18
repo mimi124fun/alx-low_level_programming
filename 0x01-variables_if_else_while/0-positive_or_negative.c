b43b6d744e0849ad21cd5039847692ec7cb8f79b
oumaima <nkihy8ws@gmail.com>
2023-02-17 17:27:58 -0800

    task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() -  RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -24,4 +24,3 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
-
