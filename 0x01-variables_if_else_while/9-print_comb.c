a36d9582b518874f693e45a0347ac3cbd44dfe05
oumaima <nkihy8ws@gmail.com>
2023-02-20 07:45:24 -0800

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - prints all combination of single digits
+ *return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	if (n != 57)
+	putchar(',');
+	putchar('');
+	putchar(10);
+	return (0);
+
+}
