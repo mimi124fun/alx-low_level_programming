1046b943a8f6a38286894f84967d4600cbb76b15
mimi124fun <nkihy8ws@gmail.com>
2023-03-06 16:43:33 -0800

    task0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+*simple_print_buffer - prints buffer in hexa
+*@buffer: the address of memory to print
+*@size: the size of the memory to print
+*description: print buffer in hexa
+*Return: Nothing.
+*/
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
@@ -30,6 +30,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 }
 /**
 * main - check the code
+*descript: check the code
 * Return: Always 0.
 */
 int main(void)
